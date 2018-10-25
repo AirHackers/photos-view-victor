588e08df90bb828832f6b9e48419f756242a4ce9
Victor Young <vyoung415@gmail.com>
2018-10-25 11:58:54 -0700

    Restructured folders and created basic server

---
+++ server/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+// var bodyParser = require('body-parser');
+var path = require('path');
+var port = 3000;
+
+var app = express();
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+
+
+
+app.listen(port, () => {
+  console.log('listening at port: ', port)
+});
