89ebe184c722ea8d7417bdb6aa00acec8c1c3f41
Edmond Lee <techhexium@gmail.com>
2018-11-05 16:43:51 -0800

    Enable CORS for endpoint

--- server/index.js
+++ server/index.js
@@ -7,12 +7,19 @@ const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 
+// Allow CORS for a given endpoint
+const allowCORS = function(res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+}
+
 app.get('/photos/:propertyID', (req, res) => {
   getFromDB(req.params.propertyID, (err, results) => {
     if (err) {
       res.status(400);
       res.send(err);
     } else {
+      allowCORS(res);
       res.status(200);
       res.send(results);
     }
